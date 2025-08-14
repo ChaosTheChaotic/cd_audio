2810fad869d1eb5fd53c6526702c0249f49c4d59
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-08-14 03:14:59 +0100

    Bro please

--- build.rs
+++ build.rs
@@ -9,6 +9,8 @@ fn main() {
     pkg_config::Config::new().probe("libcdio_cdda").expect("libcdio_cdda not found");
     pkg_config::Config::new().probe("libcdio").expect("libcdio library not found");
 
+    println!("cargo:rustc-link-lib=cdio_cdda");
+
     println!("cargo:rerun-if-changed=c_src/cd.c");
     println!("cargo:rerun-if-changed=c_src/cd.h");
 }
